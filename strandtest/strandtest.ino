2fcc00c684cef615b1cdb11272e5b319dcc1e04f
Jonas Johansson <jns.johansson@gmail.com>
2021-05-16 19:26:18 +0200

    Update strandtest.ino

--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -2,14 +2,14 @@
 
 #define LED_COUNT 360
 #define LIT_COUNT 360
-#define BRIGHTNESS 50
+#define BRIGHTNESS 192
 #define DEBUG true
 
 Adafruit_NeoPixel strips[] = {
-  Adafruit_NeoPixel(LED_COUNT, 2, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(LED_COUNT, 3, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(LED_COUNT, 4, NEO_GRB + NEO_KHZ800),
-  Adafruit_NeoPixel(LED_COUNT, 5, NEO_GRB + NEO_KHZ800),
+  Adafruit_NeoPixel(LED_COUNT, 6, NEO_GRB + NEO_KHZ800),
+  //Adafruit_NeoPixel(LED_COUNT, 7, NEO_GRB + NEO_KHZ800),
+  //Adafruit_NeoPixel(LED_COUNT, 8, NEO_GRB + NEO_KHZ800),
+  //Adafruit_NeoPixel(LED_COUNT, 9, NEO_GRB + NEO_KHZ800),
 };
 
 #define NUMSTRIPS (sizeof(strips)/sizeof(strips[0]))
@@ -31,10 +31,12 @@ void loop() {
   turnOff();
   if (DEBUG) Serial.println("Turn off!");
   delay(2000);
+  rainbow(10);
 }
 
-void turnOn(){
+void turnOn() {
   for (int i = 0; i < NUMSTRIPS; i++) {
+    //Adafruit_NeoPixel strip = strips[i];
     for (int j = 0; j < LIT_COUNT; j++) {
       strips[i].setPixelColor(j, strips[i].Color(255, 0, 0));
     }
@@ -43,11 +45,26 @@ void turnOn(){
 }
 
 
-void turnOff(){
+void turnOff() {
   for (int i = 0; i < NUMSTRIPS; i++) {
+    //Adafruit_NeoPixel strip = strips[i];
     for (int j = 0; j < LIT_COUNT; j++) {
       strips[i].setPixelColor(j, strips[i].Color(0, 0, 255));
     }
     strips[i].show();
+  }
+}
+
+void rainbow(int wait) {
+  for (int i = 0; i < NUMSTRIPS; i++) {
+    //Adafruit_NeoPixel strip = strips[i];
+    for (long firstPixelHue = 0; firstPixelHue < 5 * 65536; firstPixelHue += 256) {
+      for (int j = 0; j < strips[i].numPixels(); j++) {
+        int pixelHue = firstPixelHue + (j * 65536L / strips[i].numPixels());
+        strips[i].setPixelColor(j, strips[i].gamma32(strips[i].ColorHSV(pixelHue)));
+      }
+      strips[i].show();
+      delay(wait);
+    }
   }
 }
