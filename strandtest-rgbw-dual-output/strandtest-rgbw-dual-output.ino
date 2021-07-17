ad8b1a093d7b1bdf7ccb79b98a2a6b67d4144f67
Jonas Johansson <jns.johansson@gmail.com>
2021-07-17 22:56:11 +0200

    Create strandtest-rgbw-dual-output.ino

---
+++ strandtest-rgbw-dual-output/strandtest-rgbw-dual-output.ino
@@ -0,0 +1,57 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN     6
+#define LED_COUNT   400
+#define BRIGHTNESS  40
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+
+void setup() {
+  strip.begin(); 
+  strip.show();
+  strip.setBrightness(BRIGHTNESS);
+}
+
+void loop() {
+  colorWipe(strip.Color(  255,   0,   0, 0), 5);
+  colorWipe(strip.Color(  0,   255,   0, 0), 5);
+  colorWipe(strip.Color(  0,   0,   255, 0), 5);
+  colorWipe(strip.Color(  255,   0,   0, 255), 5);
+  colorWipe(strip.Color(  0,   255,   0, 255), 5);
+  colorWipe(strip.Color(  0,   0,  255, 255), 5);
+  colorWipe(strip.Color(  0,   0,   0, 255), 5);
+}
+
+void colorWipe(uint32_t color, int wait) {
+  for(int i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, color);
+    strip.show(); 
+    delay(wait);
+  }
+}
+
+void rainbowFade(int wait, int rainbowLoops, int whiteLoops) {
+  int fadeVal=0, fadeMax=100;
+  for(uint32_t firstPixelHue = 0; firstPixelHue < rainbowLoops*65536;
+    firstPixelHue += 256) {
+
+    for(int i=0; i<strip.numPixels(); i++) {
+      uint32_t pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue, 255,
+        255 * fadeVal / fadeMax)));
+    }
+
+    strip.show();
+    delay(wait);
+
+    if(firstPixelHue < 65536) {                              // First loop,
+      if(fadeVal < fadeMax) fadeVal++;                       // fade in
+    } else if(firstPixelHue >= ((rainbowLoops-1) * 65536)) { // Last loop,
+      if(fadeVal > 0) fadeVal--;                             // fade out
+    } else {
+      fadeVal = fadeMax; // Interim loop, make sure fade is at max
+    }
+  }
+
+  delay(500);
+}
